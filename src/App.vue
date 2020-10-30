a858600f8e75651e85db9b0e436c60309117d12b
Simon <marenkovSG@yandex.ru>
2020-10-30 23:06:32 +0300

    Update page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h2>Developer</h2>
-    <h1>Simon Marenkov</h1>
+    <AboutMe/>
   </div>
 </template>
 
 <script>
+import AboutMe from './components/AboutMe.vue'
+
 export default {
   name: 'App',
+  components: {
+    AboutMe
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="row center-xs middle-xs">
+
+    <div class="main-block col-xs-12 col-md-10 col-lg-8">
+
+      <div class="name-block">
+        <span>S</span><span>imon</span>
+        <span>Marenkov</span>
+      </div>
+
+      <div class="row center-md skills-box">
+
+        <div class="col-xs-12 col-sm">
+
+          <h2>Web developer</h2>
+
+          <img class="icon" src="../assets/img/icon/javascript.png" alt="javascript-icon">
+          <img class="icon" src="../assets/img/icon/vue.png" alt="vue-icon">
+          <img class="icon" src="../assets/img/icon/java.png" alt="java-icon">
+        </div>
+
+        <div class="col-xs-12 col-sm">
+
+          <h2>Mobile developer</h2>
+
+          <img class="icon" src="../assets/img/icon/flutter.png" alt="flutter-icon">
+          <img class="icon" src="../assets/img/icon/dart.png" alt="dart-icon">
+        </div>
+
+        <div class="col-xs-12 col-sm">
+          <h2>Game developer</h2>
+
+          <img class="icon" src="../assets/img/icon/unity.png" alt="unity-icon">
+          <img class="icon" src="../assets/img/icon/c-sharp.png" alt="c-sharp-icon">
+        </div>
+      </div>
+
+      <hr class="contact-me-line">
+
+      <div class="row center-xs contact-me">
+<!--        GitHub-->
+        <div class="icon">
+          <a href="https://github.com/smarenkov">
+            <img src="../assets/img/icon/github.png" alt="GitHub-icon">
+          </a>
+        </div>
+<!--        LinkedIn-->
+        <div class="icon">
+          <a href="https://www.linkedin.com/in/semyon-marenkov-b14078173">
+            <img src="../assets/img/icon/linkedin.png" alt="LinkedIn-icon">
+          </a>
+        </div>
+<!--        Telegram-->
+        <div class="icon">
+          <a href="https://t.me/smarenkov">
+            <img src="../assets/img/icon/telegram.png" alt="Telegram-icon">
+          </a>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutMe',
+}
+</script>
+
+<style>
+  @import "../assets/css/style.css";
+</style>
