c1b4450096327572b32f6aa52f5acc0217ea5c06
Simon Marenkov <simon.marenkov@gmail.com>
2023-03-13 02:27:57 +0200

    Edited filesort screen

--- src/components/FileSort.vue
+++ src/components/FileSort.vue
@@ -1,3 +1,8 @@
 <template>
-    <div>Text</div>
+    <div class="row center-xs middle-xs">
+        <div>
+            <img style="max-width: 1000px;" src="../assets/img/filesort/preview.png" alt="filesort_preview">
+            <img style="max-width: 1000px;" src="../assets/img/filesort/scheme_of_work.png" alt="filesort_scheme_of_work">
+        </div>
+    </div>
 </template>
