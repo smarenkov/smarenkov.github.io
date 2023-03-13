2657c0cebe959e9e04ae7a49fd7da680750cdbbe
Simon Marenkov <simon.marenkov@gmail.com>
2023-03-13 02:12:29 +0200

    gh-page

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AboutMe from './components/AboutMe'
 import FileSort from './components/FileSort'
 
@@ -16,7 +16,7 @@ const _routes = [
   ]
   
   const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: _routes
   })
   
